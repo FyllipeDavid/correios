f8880aa8b15a3f7f6d5dad9c4d7b887da19cfa96
Joelson <joelsonm@live.com>
2017-09-11 09:15:44 -0300

    arquivos de configurações

--- src/config/correios.php
+++ src/config/correios.php
@@ -1,19 +1,21 @@
 <?php
 
 return [
-    'nCdEmpresa' => '',
-    'sDsSenha' => '',
-    'sCepOrigem' => '',
-    'sCepDestino' => '',
-    'nVlPeso' => '',
-    'nCdFormato' => '',
-    'nVlComprimento' => '',
-    'nVlAltura' => '',
-    'nVlLargura' => '',
-    'nVlDiametro' => '',
-    'sCdMaoPropria' => '',
-    'nVlValorDeclarado' => '',
-    'sCdAvisoRecebimento' => '',
-    'StrRetorno' => '',
-    'nCdServico' => '', //40010,40045,40215,40290,41106
+    'company' => '', // código e senha da empresa, se você tiver contrato com os correios, se não tiver deixe vazio
+    'password' => '', // senha empresa
+    'services' => [], // serviços disponíveis
+    'address' => [
+        'zipcode' => '',
+        'street' => '',
+        'number' => '',
+        'complement' => '',
+        'district' => '',
+        'city' => '',
+        'uf' => '',
+    ],
+    'options' => [
+        'declared_value' => 0, // caso de sua encomenda extraviar, então você poderá recuperar o valor dela. Vale lembrar que o valor da encomenda interfere no valor do frete
+        'receipt_notification' => true, // receber notificações de entrega
+        'byhand' => true // aqui você informa se quer que a encomenda deva ser entregue somente para uma determinada pessoa após confirmação por RG.
+    ]
 ];
